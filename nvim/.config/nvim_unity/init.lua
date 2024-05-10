8bd12746b06dfe3891511372d3459233d20de35a
Nikola Milovic <nikolamilovic2001@gmail.com>
2024-05-10 19:34:39 +0200

    harpoon unity

--- nvim/.config/nvim_unity/init.lua
+++ nvim/.config/nvim_unity/init.lua
@@ -17,6 +17,34 @@ end
 
 vim.notify("Loaded nvim config")
 
-map("n", "<leader><leader>", function()
-  vscode.call("workbench.action.quickOpen")
-end, { remap = true })
+-- map("n", "<leader><leader>", function()
+--   vscode.call("workbench.action.quickOpen")
+-- end, { remap = true })
+--
+-- map("n", "<leader>/", function()
+--   vscode.call("workbench.action.findInFiles")
+-- end, { remap = true })
+
+-- Harpoon
+map("n", "<C-e>", function()
+  vim.notify("Harpoon c-e")
+  -- vscode.call("vscode-harpoon.editEditors")
+end, {})
+-- map("n", "<leader>a", function()
+--   vscode.call("vscode-harpoon.addEditor")
+-- end, { remap = true })
+--
+-- map("n", "<c-h>", function()
+--   vscode.call("vscode-harpoon.gotoEditor1")
+-- end, { remap = true })
+-- map("n", "<c-t>", function()
+--   vscode.call("vscode-harpoon.gotoEditor2")
+-- end, { remap = true })
+-- map("n", "<c-n>", function()
+--   vim.notify("Harpoon c-n")
+--   vscode.call("vscode-harpoon.gotoEditor3")
+-- end, { remap = true })
+-- map("n", "<c-s>", function()
+--   vim.notify("Harpoon c-s")
+--   vscode.call("vscode-harpoon.gotoEditor4")
+-- end, { remap = true })
