70d0bb0b3e99482c3dc541f7f0f49caeaf0cea69
Nikola Milovic <nikolamilovic2001@gmail.com>
2024-06-18 16:16:28 +0200

    minor updates

--- scripts/scripts/backup.sh
+++ scripts/scripts/backup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Destination directory on the external hard drive
-DEST_DIR="/home/nikola/backuphdd/backups"
+DEST_DIR="/media/nikola/BackupDrive/backups"
 
 # Exclude patterns
-EXCLUDE_PATTERNS=("node_modules/" ".turbo/" ".next/" ".pnpm-store/" "*cache*" ".npm" ".postgres-data" "*cargo*" "docker" ".steam" "Trash" ".docker" "yarn" "pnpm-store" "unityhub" "backuphdd")
+EXCLUDE_PATTERNS=("node_modules/" ".turbo/" ".next/" ".pnpm-store/" "*cache*" ".npm" ".postgres-data" "*cargo*" "docker" ".steam" "Trash" ".docker" "yarn" "pnpm-store" "unityhub" "backuphdd" "vanja/" "BackupDrive/")
 
 # Construct the --exclude options for rsync
 EXCLUDES=()
@@ -12,15 +12,40 @@ for pattern in "${EXCLUDE_PATTERNS[@]}"; do
 	EXCLUDES+=("--exclude=$pattern")
 done
 
-# Backup /home/nikola to the linux directory on the external drive
-# Uncomment the next lines if you want to backup /home/nikola too
-# echo "Starting backup of /home/nikola..."
-# rsync -avh --progress "${EXCLUDES[@]}" /home/nikola/ "$DEST_DIR/linux/"
-# echo "Backup of /home/nikola complete."
+# # Disk space check function
+# check_disk_space() {
+# 	REQUIRED_SPACE=$(du -s /home/nikola /mnt/hddstorage/files | awk '{sum += $1} END {print sum}')
+# 	AVAILABLE_SPACE=$(df "$DEST_DIR" | awk 'NR==2 {print $4}')
+#
+# 	if ((REQUIRED_SPACE > AVAILABLE_SPACE)); then
+# 		echo "Error: Not enough disk space available for the backup." | tee -a "$LOG_FILE"
+# 		exit 1
+# 	fi
+# }
+#
+# # Check disk space before starting the backup
+# check_disk_space
 
-# Backup /mnt/hddstorage/files to the hdd directory on the external drive
-echo "Starting backup of /mnt/hddstorage/files..."
-rsync -avh --progress "${EXCLUDES[@]}" /mnt/hddstorage/files/ "$DEST_DIR/hdd/"
-echo "Backup of /mnt/hddstorage/files complete."
+# Backup function with error handling
+backup() {
+	SOURCE=$1
+	DEST=$2
+	echo "Starting backup of $SOURCE..."
+	rsync -avh --progress "${EXCLUDES[@]}" "$SOURCE" "$DEST"
+	if [ $? -ne 0 ]; then
+		echo "Error: Backup of $SOURCE failed." | tee -a "$LOG_FILE"
+		exit 1
+	fi
+	echo "Backup of $SOURCE complete." | tee -a "$LOG_FILE"
+}
+
+# Backup /home/nikola
+backup "/home/nikola/" "$DEST_DIR/linux/"
+
+# Backup /mnt/hddstorage/files
+backup "/mnt/hddstorage/files/" "$DEST_DIR/hdd/"
 
 echo "All backups completed!"
+
+# Optional: Email notification (uncomment and configure if needed)
+# mail -s "Backup completed" your-email@example.com < "$LOG_FILE"
