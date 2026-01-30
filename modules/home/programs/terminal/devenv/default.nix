f4e7ca2d5fd5d2560189dcdf318fd2f5def22a79
Nikola Milovic <nikolamilovic2001@gmail.com>
2026-01-30 08:07:43 +0100

    update flakes

--- modules/home/programs/terminal/devenv/default.nix
+++ modules/home/programs/terminal/devenv/default.nix
@@ -23,5 +23,25 @@ in
       enableBashIntegration = true;
       nix-direnv.enable = true;
     };
+
+    home.file.".direnvrc".text = ''
+      # Example: export_alias zz "ls -la"
+      export_alias() {
+        local name=$1
+        shift
+        local alias_dir=$PWD/.direnv/aliases
+        local target="$alias_dir/$name"
+
+        mkdir -p "$alias_dir"
+        PATH_add "$alias_dir"
+
+        echo "#!/usr/bin/env bash" > "$target"
+        echo "set -e" >> "$target"
+        # We append "\$@" so the script passes its own arguments to the command
+        echo "$@ \"\$@\"" >> "$target"
+
+        chmod +x "$target"
+      }
+    '';
   };
 }
