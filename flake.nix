1f1bfba5f0478bcf4a69d4c8cd487311df37e82e
Nikola Milovic <nikolamilovic2001@gmail.com>
2026-04-28 10:18:08 +0200

    feat: update flake

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
     catppuccin-cursors.url = "github:catppuccin/cursors";
 
     catppuccin = {
-      url = "github:catppuccin/nix";
+      url = "github:catppuccin/nix/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
