2dd77dad02425503e18b76241bbe2e9fe2465dec
Nikola Milovic <nikolamilovic2001@gmail.com>
2026-07-03 12:37:34 +0200

    better vim

--- modules/home/programs/terminal/vim/default.nix
+++ modules/home/programs/terminal/vim/default.nix
@@ -17,6 +17,11 @@ in
   };
 
   config = mkIf cfg.enable {
+    home.packages = with pkgs; [
+      ripgrep
+      vifm
+    ];
+
     programs.vim = {
       enable = true;
       packageConfigurable = pkgs.vim-full;
@@ -24,6 +29,8 @@ in
         fzf-vim
         catppuccin-vim
         vim-dirvish
+        nerdtree
+        vifm-vim
       ];
       extraConfig = ''
         let mapleader = " "
@@ -37,12 +44,17 @@ in
 
         colorscheme catppuccin_macchiato
 
+        let g:vifm_exec = '${lib.getExe pkgs.vifm}'
+
         let g:loaded_netrwPlugin = 1
         command! -nargs=? -complete=dir Explore Dirvish <args>
         command! -nargs=? -complete=dir Sexplore belowright split | silent Dirvish <args>
         command! -nargs=? -complete=dir Vexplore leftabove vsplit | silent Dirvish <args>
 
         " Keep the fzf picker readable regardless of the active highlight defaults.
+        let s:fzf_default_opts = get(environ(), 'FZF_DEFAULT_OPTS', "")
+        let $FZF_DEFAULT_OPTS = trim(s:fzf_default_opts . ' --height=80% --layout=reverse --border --color=fg:#cad3f5,bg:#24273a,hl:#8aadf4,fg+:#cad3f5,bg+:#363a4f,hl+:#8aadf4,info:#a5adcb,prompt:#8aadf4,pointer:#8aadf4,marker:#8aadf4,spinner:#8aadf4,header:#a5adcb,border:#6e738d')
+
         highlight FzfNormal guifg=#cad3f5 guibg=#24273a ctermfg=189 ctermbg=237
         highlight FzfSelection guifg=#cad3f5 guibg=#363a4f ctermfg=189 ctermbg=238
         highlight FzfAccent guifg=#8aadf4 guibg=#24273a ctermfg=111 ctermbg=237
@@ -74,7 +86,12 @@ in
         endfunction
 
         nnoremap <silent> <leader><leader> :call <SID>OpenFilePicker()<CR>
+        nnoremap <silent> <leader>/ :RG<CR>
         nnoremap <silent> <leader>e :Explore<CR>
+        nnoremap <silent> <leader>E :NERDTreeToggle<CR>
+        nnoremap <silent> <leader>n :NERDTreeToggle<CR>
+        nnoremap <silent> <leader>N :NERDTreeFind<CR>
+        nnoremap <silent> <leader>v :Vifm<CR>
       '';
     };
   };
